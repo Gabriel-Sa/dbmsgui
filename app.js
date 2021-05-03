0894c27986c31b1f80dc9059f73686032ef68a38
Michaela Hay <mickeyhey1998@gmail.com>
2021-05-03 13:12:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,46 @@ const express = require('express');
 const bodyparser = require("body-parser");
 const path = require('path');
 const { Client } = require('pg');
+const { Router } = require('express');
+const { Pool } = require('pg');
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
 
+global.document = new JSDOM("availableVehicles.html").window.document;
 //start connection with postgresql
 
 const client = new Client({
-  user: 'root', // Change username here
+  user: 'postgres', // Change username here
   host: 'localhost',
-  database: 'project',
-  password: 'root1234', //change password here
+  database: 'Car_Rental',
+  password: 'test', //change password here
   port: 5432
 });
 
 client.connect();
+
+function completeUserInput(query) {
+  const inputQuery = query;
+  // Implement adding of the customer data to the user database based off input.
+  return inputQuery;
+}
+// const query =
+//   `CREATE TABLE users (
+//     email varchar,
+//     firstName varchar,
+//     lastName varchar,
+//     age int
+// );`;
+//
+// //Start database server
+// client.query(query, (err, res) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log('CREATE TABLE');
+//   client.end();
+// });
 
 const app = express();
 const port = 8000;
@@ -69,28 +97,6 @@ app.post("/addVehicle", (req, res) => {
   res.redirect('addVehicle.html');
 });
 
-app.post("/returnVehicle", (req, res) => {
-  var queryInput = new Array();
-  queryInput[0] = req.body.custID;
-  queryInput[1] = req.body.vehicleID;
-  queryInput[2] = req.body.vehicleDesc;
-  queryInput[3] = req.body.returnDate;
-
-  console.log(queryInput);
-  const query =
-    `SELECT * FROM vehicle AS V JOIN rental AS R ON R.VehicleID = V.VehicleID
-   WHERE V.Category = '${queryInput[2]}';`;
-  client.query(query, (err, res) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(res);
-    client.end();
-  });
-  res.redirect('returnVehicle.html');
-});
-
 
 app.post("/addReservation", (req, res) => {
   var queryInput = new Array();
@@ -98,22 +104,41 @@ app.post("/addReservation", (req, res) => {
   queryInput[1] = req.body.category;
   console.log(queryInput);
   const query =
-    `SELECT V.VehicleID as VIN, V.Description, V.Year
-   FROM vehicle AS V LEFT JOIN rental AS R ON R.VehicleID = V.VehicleID
+  `SELECT V.VehicleID as VIN, V.Description, V.Year 
+   FROM vehicle AS V LEFT JOIN rental AS R ON R.VehicleID = V.VehicleID 
    WHERE V.Category = '${queryInput[1]}' AND V.Type = '${queryInput[0]}' AND R.VehicleID IS NULL;`;
-  client.query(query, (err, res) => {
+  pool.query(query, (err, res) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log("Output,", res.rows);
-    console.log("RowCount:", res.rowCount);
+
+    //console.log("Output,", res.rows);
+    //console.log("RowCount:", res.rowCount);
     //client.end();
   });
+  //document.getElementByID('printVehicle').innerHTML = "testing";
   res.redirect('availableVehicles.html');
-  //res.render('tempdisplay', {data: res.rows});
+  let table = document.getElementById("availableVehicle");
+  //document.write("test");
+  let data = Object.keys("vin", "description", "year");
+  console.log(table);
+  generateTableHead(table, data);
+
+  console.log(table);
+  console.log(data);
 });
 
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
 
 app.listen(port, () => {
   console.log(`App started listening at http://localhost:${port}`);
